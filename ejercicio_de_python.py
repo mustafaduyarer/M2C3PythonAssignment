c71476848685ccc7b94134e1706bf363dc345cc6
mustafaduyarer <mustafaduyarer@gmail.com>
2024-03-20 22:40:31 +0100

    ejercicio added

---
+++ ejercicio_de_python.py
@@ -0,0 +1,10 @@
+
+# ejercicio de Python
+
+cadena_original = "Hola python"
+print(cadena_original)
+
+find_hola = cadena_original.find("Hola")
+
+cadena_modificada = cadena_original.replace("Hola", "adiós")
+print(cadena_modificada)
